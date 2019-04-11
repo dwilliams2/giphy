c400bece3efbe9e688cabdb55e4c84b4f6347130
dwilliams2 <dwilliams1@westinghousehs.org>
2019-04-11 22:24:25 +0000

    cat

--- js/script.js
+++ js/script.js
@@ -2,13 +2,16 @@
 //******************TEST EARLY AND OFTEN USING console.log() ******************
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
 /* global $ */
- 
+
 
 $("#search-button").click(function(){
+    var choise= $("input").val();
   $.ajax({
-        url: "https://api.giphy.com/v1/gifs/search?q=puppy&rating=pg&api_key=dc6zaTOxFJmzC",
+
+        url: "https://api.giphy.com/v1/gifs/search?q="+choise +"&rating=pg&api_key=dc6zaTOxFJmzC",
         method: "GET",
         success: function(response) {
-          console.log(response.data); 
+         $('.text-center').append("<img src=" + response.data[0].images.fixed_width.url + ">");
+
         }});
 });
